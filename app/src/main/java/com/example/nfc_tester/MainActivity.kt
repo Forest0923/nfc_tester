626d45926220633bdf731f0c57867e7544430413
Forest0923 <tropicalrainforest23@gmail.com>
2022-11-03 21:21:27 +0900

    Feat: Implement Felica commands
    - polling
    - requestService
    - requestResponse
    - readWithoutEncryption
    - searchServiceCode
    - requestSystemCode

--- app/src/main/java/com/example/nfc_tester/MainActivity.kt
+++ app/src/main/java/com/example/nfc_tester/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.nfc_tester
 
 import android.content.ContentValues.TAG
 import android.content.Intent
-import android.nfc.*
-import android.nfc.tech.*
-import androidx.appcompat.app.AppCompatActivity
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.NfcF
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,37 +21,149 @@ class MainActivity : AppCompatActivity() {
 
         val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG) ?: return;
         Log.d(TAG, tag.toString())
-        val result  = NfcFReader().read(tag)
-        val resultView  = findViewById<TextView>(R.id.result)
+//        val result = NfcFReader().read(tag)
+        val result = read(tag)
+        val resultView = findViewById<TextView>(R.id.result)
         resultView.text = result.joinToString(separator = "\n")
     }
-}
 
-class NfcFReader() {
-    fun read(tag: Tag): Array<String> {
-        val nfc = NfcF.get(tag)
-        val id = tag.id.joinToString(separator = ":") { eachByte -> "%02X".format(eachByte) }
-        val systemcode =
-            nfc.systemCode.joinToString(separator = ":") { eachByte -> "%02X".format(eachByte) }
-        val pmm =
-            nfc.manufacturer.joinToString(separator = ":") { eachByte -> "%02X".format(eachByte) }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    private fun read(tag: Tag): Array<String> {
+        val nfcf: NfcFReader = NfcFReader(tag)
+
         try {
-            nfc.connect()
-//            val command = byteArrayOf(0x00.toByte())
-//            val IDm     = tag.id
-//            val msg = byteArrayOf(command.size.toByte(), command[0], command[1])
-//            Log.d(TAG, "${msg[0]}, ${msg[1]}, ${msg[2]}")
-//            val res = nfc.transceive(msg)
-//            Log.d(TAG, "res = $res")
+            nfcf.nfc.connect()
 
-            nfc.close()
+            nfcf.polling()?.let { printByteArray("polling: res\t=", it) }
+            nfcf.requestResponse()?.let { printByteArray("requestResponse: res\t=", it) }
+            nfcf.readWithoutEncryption(byteArrayOf(0x20, 0x4B), 1)
+                ?.let { printByteArray("readWithoutEncryption: res\t=", it) }
+            nfcf.requestSystemCode()?.let { printByteArray("requestSystemCode: res\t=", it) }
+
+            for (i: Int in 0..0xffff) {
+                val res = nfcf.searchServiceCode(byteArrayOf((i shl 8).toByte(), (i and 0xff).toByte()))
+                    ?: return emptyArray()
+                Log.d(TAG, "0x%02X(%d), 0x%02X, 0x%02X".format(i, i, i shr 8, i and 0xff))
+                printByteArray("searchServiceCode: res\t=", res)
+            }
+
+            nfcf.nfc.close()
         } catch (e: Exception) {
             Log.e(TAG, "NfcFReader: ERROR '$e'")
-            if (nfc.isConnected) {
-                nfc.close()
+            if (nfcf.nfc.isConnected) {
+                nfcf.nfc.close()
             }
         }
 
-        return arrayOf(tag.toString(), "ID  = $id", "PMm = $pmm", "SystemCode  = $systemcode")
+        return arrayOf(tag.toString(), "ID  = ${nfcf.idm}", "PMm = ${nfcf.pmm}", "SystemCode  = ${nfcf.systemCode}")
+    }
+
+    private fun printByteArray(prefix: String, array: ByteArray) {
+        Log.d(
+            TAG,
+            "$prefix ${array.joinToString(separator = ":") { eachByte -> "%02X".format(eachByte) }}"
+        )
+    }
+}
+
+class NfcFReader(tag: Tag) {
+    val idm: ByteArray
+    val nfc: NfcF
+    val systemCode: ByteArray
+    val pmm: ByteArray
+
+    init {
+        idm = tag.id
+        nfc = NfcF.get(tag)
+        systemCode  = nfc.systemCode
+        pmm = nfc.manufacturer
+    }
+
+    private fun printByteArray(prefix: String, array: ByteArray) {
+        Log.d(
+            TAG,
+            "$prefix ${array.joinToString(separator = ":") { eachByte -> "%02X".format(eachByte) }}"
+        )
+    }
+
+    fun polling(): ByteArray? {
+        val cmdCode     = byteArrayOf(0x00)
+        val reqCode     = byteArrayOf(0x00)
+        val timeSlot    = byteArrayOf(0x0f)
+
+        val command = cmdCode + systemCode + reqCode + timeSlot
+        printByteArray("polling: command\t=", command)
+        val msg = byteArrayOf((command.size + 1).toByte()) + command
+        printByteArray("polling: msg\t=", msg)
+        return nfc.transceive(msg)
+    }
+
+    fun requestService(): ByteArray? {
+        val cmdCode         = byteArrayOf(0x02)
+        val nodeNum         = byteArrayOf(0x01)
+        val nodeCodeList    = byteArrayOf(0x08, 0x20)
+
+        val command = cmdCode + idm + nodeNum + nodeCodeList
+        printByteArray("requestService: command\t=", command)
+        val msg = byteArrayOf((command.size + 1).toByte()) + command
+        printByteArray("requestService: msg\t=", msg)
+        return nfc.transceive(msg)
+    }
+
+    fun requestResponse(): ByteArray? {
+        val cmdCode         = byteArrayOf(0x04)
+
+        val command = cmdCode + idm
+        printByteArray("requestResponse: command\t=", command)
+        val msg = byteArrayOf((command.size + 1).toByte()) + command
+        printByteArray("requestResponse: msg\t=", msg)
+
+        return nfc.transceive(msg)
+    }
+
+    fun readWithoutEncryption(serviceCode: ByteArray, size: Int): ByteArray? {
+        val cmdCode         = byteArrayOf(0x06) // (1-byte)
+        val serviceNum      = byteArrayOf(0x01) // 1~16 (1-byte)
+        val serviceCodeList = byteArrayOf(serviceCode[1], serviceCode[0])    // 0x000B 0x0009 (2m-byte)
+        val blockNum        = byteArrayOf(size.toByte()) // (1-byte)
+        var blockList       = byteArrayOf()     // (N-byte, 2n <= N <= 3n)
+        for (i in 0 until size) {
+            blockList += byteArrayOf(0x80.toByte(), i.toByte())
+        }
+
+        val command = cmdCode + idm + serviceNum + serviceCodeList + blockNum + blockList
+        printByteArray("readWithoutEncryption: command\t=", command)
+        val msg = byteArrayOf((command.size + 1).toByte()) + command
+        printByteArray("readWithoutEncryption: msg\t=", msg)
+
+        return nfc.transceive(msg)
+    }
+
+    fun searchServiceCode(index: ByteArray): ByteArray? {
+        val cmdCode = byteArrayOf(0x0A)
+        val idx     = byteArrayOf(index[1], index[0])
+
+        val command = cmdCode + idm + idx
+        val msg     = byteArrayOf((command.size + 1).toByte()) + command
+
+        printByteArray("searchServiceCode: msg\t=", msg)
+        return nfc.transceive(msg)
+    }
+
+    fun requestSystemCode(): ByteArray? {
+        val cmdCode = byteArrayOf(0x0C)
+
+        val command = cmdCode + idm
+        val msg = byteArrayOf((command.size + 1).toByte()) + command
+        return nfc.transceive(msg)
     }
 }
